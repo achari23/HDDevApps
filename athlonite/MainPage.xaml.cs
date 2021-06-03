5543c3413735dac9ae668085a60ef8dbb2f6556d
Anand Chari <achari@purdue.edu>
2021-06-03 11:55:33 -0400

    swipe mechanism implemented

--- athlonite/MainPage.xaml.cs
+++ athlonite/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -13,9 +14,49 @@ namespace athlonite
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<UserProfile> _Profiles = new ObservableCollection<UserProfile>();
         public MainPage()
         {
             InitializeComponent();
+            CardBinding();
+            BindingContext = this;
+        }
+        //binding the cards. is also the dataset for the cards
+        public void CardBinding()
+        {
+            Profiles.Add(new UserProfile(){ Name = "Anand", Age = "19", Photo = "promprofile.jpg" });
+            Profiles.Add(new UserProfile(){ Name = "Joy", Age = "184", Photo = "helps.PNG" });
+            Profiles.Add(new UserProfile(){ Name = "Vasu", Age = "185", Photo = "promprofile.jpg" });
+            Profiles.Add(new UserProfile(){ Name = "Rylan", Age = "198", Photo = "Screenshot (3).png" });
+            Profiles.Add(new UserProfile(){ Name = "Maya", Age = "168", Photo = "promprofile.jpg" });
+            Profiles.Add(new UserProfile(){ Name = "Johnnie", Age = "128", Photo = "promprofile.jpg" });
+            Profiles.Add(new UserProfile(){ Name = "Jackie", Age = "138", Photo = "promprofile.jpg" });
+        }
+        //Gives values to the profile, allows profile to retrieve inputs
+        public ObservableCollection<UserProfile> Profiles
+        {
+            get => _Profiles;
+            set
+            {
+                _Profiles = value;
+            }
+        }
+        //user profile: has name, age, photo currently. Very easy to change from here
+        public class UserProfile 
+        {
+            public string Name { get; set; }
+            public string Age { get; set; }
+            public string Photo { get; set; }
+
+        }
+        //Handlers for buttons
+        private void OnDislikeClicked(object sender, EventArgs e)
+        {
+
+        }
+        private void OnLikeClicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- athlonite/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ athlonite/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,20 @@ namespace athlonite {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::MLToolkit.Forms.SwipeCardView.SwipeCardView SwipeView1;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button nopeButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button yesButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            SwipeView1 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::MLToolkit.Forms.SwipeCardView.SwipeCardView>(this, "SwipeView1");
+            nopeButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "nopeButton");
+            yesButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "yesButton");
         }
     }
 }
